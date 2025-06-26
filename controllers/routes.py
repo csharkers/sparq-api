b4c0caa8ffb37e3c7908e491d788c7638ede2b2e
3º MTEC - Desenv. de Sistemas <ds-mtec-3@servidor.acad>
2025-06-26 10:56:39 -0300

    Fixed json 2

--- controllers/routes.py
+++ controllers/routes.py
@@ -67,8 +67,7 @@ def init_app(app):
                     "dateserver": r.dateserver.isoformat() if r.dateserver else None,
                     "thermo_mat": json.loads(r.thermo_mat) if r.thermo_mat else None,
                     "temp_soil": (
-                        sum(val for row in json.loads(r.thermo_mat) for val in row) / 
-                        len([val for row in json.loads(r.thermo_mat) for val in row])
+                        (lambda t: sum(t) / len(t) if t else None)(json.loads(r.thermo_mat))
                         if r.thermo_mat else None
                     )
                 })
